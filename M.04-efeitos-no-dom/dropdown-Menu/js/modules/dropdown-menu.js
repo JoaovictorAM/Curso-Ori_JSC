2a97009a604daab46ea4b2e3908ac78f0c5aac98
Joao Victor A Macedo <Joaovictoram31@outlook.com>
2020-11-24 18:45:30 -0300

    Dropdown menup pt2

--- M.04-efeitos-no-dom/dropdown-Menu/js/modules/dropdown-menu.js
+++ M.04-efeitos-no-dom/dropdown-Menu/js/modules/dropdown-menu.js
@@ -1,14 +1,16 @@
 export default function initDropdownMenu() {
-}
+    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
-const dropdownMenus = document.querySelectorAll('data-dropdown');
-
-dropdownMenus.forEach(menu => {
-    menu.addEventListener('click', handleClick);
-})
-
-
-function handleClick(event) {
-    event.preventDefault();
-    console.log(event);
+    dropdownMenus.forEach(menu => {
+        menu.addEventListener('click', handleClick);
+        ['touchstart','click'].forEach(userEvent =>{
+            menu.addEventListener(userEvent, handleClick);
+        })
+    })
+    
+    
+    function handleClick(event) {
+        event.preventDefault();
+        this.classList.add('active');
+    }
 }
