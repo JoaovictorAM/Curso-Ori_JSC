d3f3faf68f1b0c786b700f531708e6117a90ec22
Joao Victor A Macedo <Joaovictoram31@outlook.com>
2020-11-30 19:19:47 -0300

    outsideclick finallizado

--- M.04-efeitos-no-dom/clickouside/js/modules/dropdown-menu.js
+++ M.04-efeitos-no-dom/clickouside/js/modules/dropdown-menu.js
@@ -1,28 +1,19 @@
+import outsideClick from './outsideclick.js';
+
 export default function initDropdownMenu() {
     const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
     dropdownMenus.forEach(menu => {
-        menu.addEventListener('click', handleClick);
-        ['touchstart','click'].forEach(userEvent =>{
+        ['touchstart', 'click'].forEach(userEvent => {
             menu.addEventListener(userEvent, handleClick);
-        })
-    })
-    
-    
+        });
+    });
+
     function handleClick(event) {
         event.preventDefault();
         this.classList.add('active');
-        outsideClick(this,()=>{
-            console.log('ativou');
+        outsideClick(this, ['touchstart', 'click'], () => {
+            this.classList.remove('active');
         });
-    }
-
-    function outsideClick(el,cb){
-        const html = document.documentElement;
-        html.addEventListener('click', handleOutsideClick);
-        function handleOutsideClick (event){
-            console.log(el);
-            cb();
-        }
     }
 }
