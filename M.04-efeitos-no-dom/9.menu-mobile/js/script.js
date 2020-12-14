2972efa3797534b5620f6314d56bffa3a4801f52
Joao Victor A Macedo <joaovictoram31@outlook.com>
2020-12-14 14:35:06 -0300

    Att na ordem das pastas

---
+++ M.04-efeitos-no-dom/9.menu-mobile/js/script.js
@@ -0,0 +1,17 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/anima-scroll.js';
+import intiAccordion from './modules/accordion.js';
+import intiTabNav from './modules/tabNav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+intiAccordion();
+intiTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
