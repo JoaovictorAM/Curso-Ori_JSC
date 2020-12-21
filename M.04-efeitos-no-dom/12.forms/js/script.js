5cfd3697c9c4b5721e91fea6321f9445d6508d4b
Joao Victor A Macedo <joaovictoram31@outlook.com>
2020-12-21 16:09:01 -0300

    Pt2 Forms

--- M.04-efeitos-no-dom/12.forms/js/script.js
+++ M.04-efeitos-no-dom/12.forms/js/script.js
@@ -1,10 +1,16 @@
 const contato = document.querySelector('#contato');
 const textoSpan = document.querySelector('.texto');
-
-function handleKeyUp(event){
+const dados = {};
+function handlechange(event){
   const target = event.target;
-  
-  console.log(event.target.value);
-  textoSpan.innerText = event.target.value;
+  // if(!target.checkValidity()){
+  //   target.classList.add('invalido');
+  //   //target.setCustomValidity('Esse campo é importante!');
+  //   target.nextElementSibling.innerText = target.validationMessage;
+  // }
+  // textoSpan.innerText = event.target.value;
+
+  //document.body.style.backgroundColor = target.value;
+  dados[target.name] = target.value;
 }
-contato.addEventListener('change', handleKeyUp);
+contato.addEventListener('change', handlechange);
