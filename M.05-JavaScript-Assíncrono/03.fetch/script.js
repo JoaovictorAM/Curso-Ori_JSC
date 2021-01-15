1d3e107eb6c2f4371ebd6acb1db62d78243a234a
Joao Victor A Macedo <joaovictoram31@outlook.com>
2021-01-15 17:26:39 -0300

    fetch HTML E .TEXT()

--- M.05-JavaScript-Assíncrono/03.fetch/script.js
+++ M.05-JavaScript-Assíncrono/03.fetch/script.js
@@ -1,8 +1,8 @@
-const doc = fetch('./style.css');
-const styleElement = document.createElement('style');
+const doc = fetch('./sobre.html');
+const div = document.createElement('div')
 doc.then(r => r.text())
 .then(body=>{
-	styleElement.innerHTML = body;
-	const conteudo = document.querySelector('.conteudo');
-	conteudo.appendChild(styleElement);
+	div.innerHTML = body;
+	const titulo = div.querySelector('h1');
+	document.querySelector('h1').innerText = titulo.innerText;
 });
