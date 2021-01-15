57a2421e3ccaec78019cbf8e9dfd81272bfeab6d
Joao Victor A Macedo <joaovictoram31@outlook.com>
2021-01-15 17:28:46 -0300

    fetch blob

--- M.05-JavaScript-Assíncrono/03.fetch/script.js
+++ M.05-JavaScript-Assíncrono/03.fetch/script.js
@@ -1,8 +1,8 @@
-const doc = fetch('./sobre.html');
-const div = document.createElement('div')
-doc.then(r => r.text())
+const doc = fetch('./img.jpg');
+
+doc.then(r => r.blob())
 .then(body=>{
-	div.innerHTML = body;
-	const titulo = div.querySelector('h1');
-	document.querySelector('h1').innerText = titulo.innerText;
+	const blobUrl = URL.createObjectURL(body);
+	const imgDom = document.querySelector('img');
+	imgDom.src = blobUrl;
 });
