4f1f8a8aa4dbdd6d1d7700ddf360fdcb56f21f91
Joao Victor A Macedo <joaovictoram31@outlook.com>
2021-01-15 17:20:19 -0300

    fecht json

--- M.05-JavaScript-Assíncrono/03.fetch/script.js
+++ M.05-JavaScript-Assíncrono/03.fetch/script.js
@@ -1,9 +1,7 @@
-const sobre = fetch('./sobre.html');
-const divEx = document.createElement('div');
-sobre.then((r)=>{
-	r.text();
-}).then((body)=>{
-	divEx.innerHTML=body;
-	const titulo = divEx.querySelector('h1');
-	document.querySelector('h1').innerText = titulo.innerText;
-})
+const doc = fetch('https://viacep.com.br/ws/01001000/json/');
+
+doc.then(r => r.json())
+.then(body=>{
+	const conteudo = document.querySelector('.conteudo');
+	conteudo.innerText = body.logradouro;
+});
