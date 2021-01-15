ca49be83b3f41c4ff3bcf9e3c5ec5c5f4316bf11
Joao Victor A Macedo <joaovictoram31@outlook.com>
2021-01-15 18:29:22 -0300

    fetch headers

--- M.05-JavaScript-Assíncrono/03.fetch/script.js
+++ M.05-JavaScript-Assíncrono/03.fetch/script.js
@@ -1,14 +1,5 @@
 const doc = fetch('https://viacep.com.br/ws/01001000/json/');
 
-doc.then(r =>{ 
-	const r2 = r.clone();
-	r.text().then((text)=>{
-		console.log(text)
-	})
-	r2.json().then((json)=>{
-		console.log(json);
-	})
-})
-.then(body=>{
-	console.log(body);
+doc.then((response) =>{ 
+	response.headers.forEach(console.log);
 });
